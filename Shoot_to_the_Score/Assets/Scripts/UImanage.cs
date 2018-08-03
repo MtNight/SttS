2e63a7bd211c4ffaea9b20655268878b0c5f49c2
Mt.Night <eskwon0228@naver.com>
2018-08-03 19:52:26 +0900

    Add Unity files

---
+++ Shoot_to_the_Score/Assets/Scripts/UImanage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanage : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update () {
+		
+	}
+}
