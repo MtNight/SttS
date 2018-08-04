bc80166bfeb36930b7b5856823eaf6b692a2fd5e
Mt.Night <eskwon0228@naver.com>
2018-08-04 19:22:39 +0900

    tiny change

---
+++ Shoot_to_the_Score/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private float Speed;
+    private Vector3 moveArrow;
+    private Vector3 dir;
+
+	void Start () {
+        Speed = 3.0f;
+        moveArrow = Vector3.zero;
+    }
+	
+	void Update () {
+        moveArrow = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveArrow = Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveArrow = Vector3.right;
+        }
+        transform.position += moveArrow * Speed * Time.deltaTime;
+	}
+}
---
+++ Shoot_to_the_Score/Assets/Scripts/Scenemoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemoving : MonoBehaviour {
+    
+	void Start () {
+        StartCoroutine("LtoM");
+	}
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update () {
+		
+	}
+
+    IEnumerator LtoM()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            SceneManager.LoadScene("GameScene");   //LoadtoMain -> 임시로 GameScene이동
+        }
+    }
+}
