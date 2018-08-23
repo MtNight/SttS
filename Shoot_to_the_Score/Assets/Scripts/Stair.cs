2b5131509788dcc58fe5dc42ead21151e8b4fec1
Mt.Night <eskwon0228@naver.com>
2018-08-23 18:52:03 +0900

    Stage1 D-4

---
+++ Shoot_to_the_Score/Assets/Scripts/Stair.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stair : MonoBehaviour {
+
+    private bool act;
+    private bool tel;
+    private GameObject player;
+
+    void Start () {
+        act = false;
+        tel = GameObject.Find("Teleport1-1").GetComponent<Teleport>().use;
+        player = GameObject.Find("Player");
+	}
+	
+	void Update ()
+    {
+        if (act == false)
+        {
+            tel = GameObject.Find("Teleport1-1").GetComponent<Teleport>().use;
+            if (tel == true)
+            {
+                player = GameObject.Find("Player");
+                if (player.transform.position.x > 15.5f && player.transform.position.y > -74)
+                {
+                    Vector3 i = transform.position;
+                    i.y -= 20;
+                    transform.position = i;
+                    act = true;
+                }
+            }
+        }
+	}
+}
--- Shoot_to_the_Score/Assets/Scripts/Teleport.cs
+++ Shoot_to_the_Score/Assets/Scripts/Teleport.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour {
 
-    private int tnum;
+    public int tnum;
+    public bool use;
+    public bool active;
 
 	void Start () {
+        use = false;
         if (transform.position.x == 36 && transform.position.y == -20.5)
         {
             tnum = 0;
+            active = true;
         }
         else if (transform.position.x == 37.8f && transform.position.y == -40.5f)
         {
             tnum = 1;
+            active = true;
+        }
+        else if (transform.position.x == 26.0f && transform.position.y == -113.0f)
+        {
+            tnum = 2;
+            active = false;
+        }
+        else if (transform.position.x == 7.0f && transform.position.y == -68.0f)
+        {
+            tnum = 3;
+            active = true;
+        }
+        else if (transform.position.x == 23.0f && transform.position.y == -101.75f)
+        {
+            tnum = 4;
+            active = false;
+        }
+        else if (transform.position.x == 50.0f && transform.position.y == -90.5f)
+        {
+            tnum = 5;
+            active = false;
         }
     }
 	
@@ -27,12 +52,18 @@ public class Teleport : MonoBehaviour {
         {
             if (other.transform.parent.gameObject.tag == "Player")
             {
-                switch (tnum)
+                if (/*active == */true)
                 {
-                    case 0: other.transform.parent.gameObject.transform.position = new Vector3(3, -45, 5); break;
-                    case 1: other.transform.parent.gameObject.transform.position = new Vector3(3, -111f, 5); break;
+                    switch (tnum)
+                    {
+                        case 0: other.transform.parent.gameObject.transform.position = new Vector3(3, -45, 5); use = true; break;
+                        case 1: other.transform.parent.gameObject.transform.position = new Vector3(3, -111, 5); use = true; break;
+                        case 2: other.transform.parent.gameObject.transform.position = new Vector3(3, -77.25f, 5); use = true; break;
+                        case 3: other.transform.parent.gameObject.transform.position = new Vector3(27, -99.75f, 5); use = true; break;
+                        case 4: other.transform.parent.gameObject.transform.position = new Vector3(23, -90.5f, 5); use = true; break;
+                        case 5: other.transform.parent.gameObject.transform.position = new Vector3(50, -79.25f, 5); use = true; break;
+                    }
                 }
-                
             }
         }
     }
